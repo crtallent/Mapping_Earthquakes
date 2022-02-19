d3cb530a4af7941dd905cd6fb4039a094d391d54
crtallent <91101467+crtallent@users.noreply.github.com>
2022-02-19 09:44:15 -0600

    Updated files.

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -6,26 +6,30 @@ let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
 // Coordinates for each point to be used in the polyline.
 let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
+    [37.615223, -122.389977],
+    [30.18999924, -97.6686639],
+    [38.74165, -90.3657],
+    [43.677717, -79.624819],
+    [40.641766, -73.780968]
   ];
 
-// Create polyline using the line coordinates and make line read.
+// Create polyline using the line coordinates and make line yellow.
 L.polyline(line, {
-    color: "yellow"
+    color: "blue",
+    weight: 4,
+    opacity: .5,
+    dashArray: "20,15",
 }).addTo(map);
 
-
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
-});
-
+});   
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
+
+                                                                                                                                                                                                                                                              
